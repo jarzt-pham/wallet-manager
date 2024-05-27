919a3ce1267895d27b21191e35804bde2d544d92
Jarzt <dev.luanpham@gmail.com>
2024-05-28 03:03:03 +0700

    feat: add validation pipe to main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Configuration } from './configuration';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  
   const configService = app.get(ConfigService);
   const appPort = configService.get('APP_PORT') || 3000;
   
