2ccf2d5eab16507455c47e533dcb4c65284193b7
Jarzt <dev.luanpham@gmail.com>
2024-05-28 03:18:35 +0700

    delete: remove barrel export for entity
    error circular dependency of nestjs

--- src/features/employee/domain/entities/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './employee.entity';
-export * from './employee-type.entity';
-
-export * from './employee-salary.entity';
-export * from './employee-salary-type.entity';
-
-export * from './employee-attendance.entity';
