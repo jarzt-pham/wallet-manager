e2dc809954285076d1667297a65fdc460776a2bc
Jarzt <dev.luanpham@gmail.com>
2024-05-28 03:23:35 +0700

    feat(employee): create an simple controller

--- src/features/employee/presentation/employee.controller.ts
+++ src/features/employee/presentation/employee.controller.ts
@@ -1,12 +1,25 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
 } from '@nestjs/common';
+import { EmployeeService } from '../infrastructure';
+import { EmployeeValidateUtils } from '../validates';
 
-@Controller('employee')
-export class EmployeeController {}
+@Controller('employees')
+export class EmployeeController {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  @Post('/')
+  async create(
+    @Body()
+    payload: EmployeeValidateUtils.CreateEmployeePayloadValidate
+  ) {
+    return this.employeeService.create({
+      name: payload.name,
+      employeeTypeId: payload.employee_type_id,
+      baseSalary: payload.base_salary,
+      balance: payload.balance,
+    });
+  }
+}
