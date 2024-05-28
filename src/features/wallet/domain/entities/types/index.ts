b6f220cb3714679e5ccabaab3d98708bc123b8e0
Jarzt <dev.luanpham@gmail.com>
2024-05-28 22:08:03 +0700

    update: implement queue and export related service, processor

---
+++ src/features/wallet/domain/entities/types/index.ts
@@ -0,0 +1,2 @@
+export * from './employee-wallet.type'
+export * from './employee-wallet-log.type';
--- src/features/wallet/wallet.module.ts
+++ src/features/wallet/wallet.module.ts
@@ -3,10 +3,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeWallet } from './domain/entities/employee-wallet.entity';
 import { EmployeeWalletLog } from './domain/entities/employee-wallet-log.entity';
 import { WalletController } from './presentation';
+import { WalletService } from './infrastructure/services/wallet.service';
+import { WalletProcessor } from './processors/wallet.processor';
+import { WalletLogProcessor } from './processors/wallet-logs.processor';
+import { EmployeeModule } from '../employee';
+import { AppModule } from 'src/app.module';
+import { BullModule } from '@nestjs/bull';
+import { Configuration } from 'src/configuration';
+import { ConfigModule } from '@nestjs/config';
+import { JobModule } from '../job';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EmployeeWallet, EmployeeWalletLog])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([EmployeeWallet, EmployeeWalletLog]),
+
+    BullModule.forRoot(Configuration.BullConfiguration.Root),
+    BullModule.registerQueue(...Configuration.BullConfiguration.Register),
+
+    EmployeeModule,
+    JobModule
+  ],
   controllers: [WalletController],
-  providers: [],
+  providers: [WalletService, WalletProcessor, WalletLogProcessor],
+  exports: [WalletProcessor, WalletLogProcessor, BullModule],
 })
 export class WalletModule {}
