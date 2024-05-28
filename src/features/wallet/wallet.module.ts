bad5a4ca6886509e29f1b4d3aa4e3c43244637b4
Jarzt <dev.luanpham@gmail.com>
2024-05-29 00:12:19 +0700

    chore: rename of wallet log processor

--- src/features/wallet/wallet.module.ts
+++ src/features/wallet/wallet.module.ts
@@ -5,7 +5,7 @@ import { EmployeeWalletLog } from './domain/entities/employee-wallet-log.entity'
 import { WalletController } from './presentation';
 import { WalletService } from './infrastructure/services/wallet.service';
 import { WalletProcessor } from './processors/wallet.processor';
-import { WalletLogProcessor } from './processors/wallet-logs.processor';
+import { WalletLogProcessor } from './processors/wallet-log.processor;
 import { EmployeeModule } from '../employee';
 import { AppModule } from 'src/app.module';
 import { BullModule } from '@nestjs/bull';
