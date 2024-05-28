f27853b4e870fc13721de775fa03af6028978739
Jarzt <dev.luanpham@gmail.com>
2024-05-28 22:09:08 +0700

    update: implement cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,17 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Configuration } from './configuration';
 import { EmployeeModule, WalletModule } from './features';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync(Configuration.TypeOrmConfiguration.Async),
 
+    ScheduleModule.forRoot(),
+
     EmployeeModule,
-    WalletModule
+    WalletModule,
   ],
   controllers: [AppController],
   providers: [AppService],
