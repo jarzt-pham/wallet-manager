7f7340c33a18481031ce13bcbfe49dffc419b41c
Jarzt <dev.luanpham@gmail.com>
2024-05-28 22:07:22 +0700

    update: create an api to test update balance feature

--- src/features/wallet/presentation/wallet.controller.ts
+++ src/features/wallet/presentation/wallet.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { WalletService } from '../infrastructure/services/wallet.service';
+import { EmployeeService } from 'src/features/employee/infrastructure';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
+import { JOB_QUEUE } from 'src/features/job/queue';
 
 @Controller('wallets')
 export class WalletController {
-  constructor() {}
+  constructor(
+    private readonly _walletService: WalletService,
+  ) {}
+
+  @Get()
+  async executeUpdateWalletBalance() {
+    await this._walletService.triggerForTesting();
+    return 'wallets';
+  }
 }
