4f56222e51726f540115fc888e4e63ab23b071c0
Jarzt <dev.luanpham@gmail.com>
2024-05-28 22:08:45 +0700

    update(cron): add cron to app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { initDataForEmployee } from './infrastructure';
 import { DataSource } from 'typeorm';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
+import { JOB_QUEUE } from './features/job/queue';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { EmployeeService } from './features/employee/infrastructure';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-  constructor(private readonly _dataSource: DataSource) {}
+  constructor(
+    private _configService: ConfigService,
+    private readonly _dataSource: DataSource,
+    private employeeService: EmployeeService,
+    @InjectQueue(JOB_QUEUE.WALLET) private _walletQueue: Queue,
+    private readonly _employeeService: EmployeeService,
+  ) {}
 
   getHello(): string {
     return 'Hello World!';
@@ -12,5 +24,28 @@ export class AppService implements OnModuleInit {
 
   onModuleInit() {
     initDataForEmployee(this._dataSource);
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async handleCron() {
+    await this.scheduleDailyCalculateAndUpdateBalance();
+  }
+
+  private async scheduleDailyCalculateAndUpdateBalance() {
+    const totalEmployees = await this._employeeService.countEmployees();
+    const USER_AMOUNT_UPDATE =
+      this._configService.get('USER_AMOUNT_UPDATE') ?? 1;
+
+    const totalBatches = Math.ceil(totalEmployees / USER_AMOUNT_UPDATE);
+
+    for (let batch = 0; batch < totalBatches; batch++) {
+      await this._walletQueue.add({
+        batch,
+        paging: {
+          limit: USER_AMOUNT_UPDATE,
+          offset: batch * USER_AMOUNT_UPDATE,
+        },
+      });
+    }
   }
 }
