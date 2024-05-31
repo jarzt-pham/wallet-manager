615a723e130c206585fea8f52401e8c16fd42b43
Jarzt <dev.luanpham@gmail.com>
2024-05-31 07:10:10 +0700

    chore(wallet): change name of func in api trigger

--- src/features/wallet/presentation/wallet.controller.ts
+++ src/features/wallet/presentation/wallet.controller.ts
@@ -1,17 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { WalletService } from '../infrastructure/services/wallet.service';
-import { EmployeeService } from 'src/features/employee/infrastructure';
-import { InjectQueue } from '@nestjs/bull';
-import { Queue } from 'bull';
-import { JOB_QUEUE } from 'src/features/job/queue';
 
 @Controller('wallets')
 export class WalletController {
-  constructor(
-    private readonly _walletService: WalletService,
-  ) {}
+  constructor(private readonly _walletService: WalletService) {}
 
-  @Get()
+  @Get('/trigger')
   async executeUpdateWalletBalance() {
     await this._walletService.triggerForTesting();
     return 'wallets';
